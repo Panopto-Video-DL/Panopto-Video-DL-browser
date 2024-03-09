003b9501bd91dd2453422e985f365930ecd94158
Davide <49226282+pizidavi@users.noreply.github.com>
2024-03-09 15:16:59 +0100

    Fixed #7
    Fixed Download button for folders

--- Panopto-Video-DL.js
+++ Panopto-Video-DL.js
@@ -4,7 +4,7 @@
 // @description  Video downloader for Panopto
 // @icon         https://www.panopto.com/wp-content/themes/panopto/library/images/favicons/favicon-96x96.png
 // @author       Panopto-Video-DL
-// @version      3.3.0
+// @version      3.3.1
 // @copyright    2021, Panopto-Video-DL
 // @license      MIT
 // @homepage     https://github.com/Panopto-Video-DL/Panopto-Video-DL-browser
@@ -23,22 +23,24 @@
 // @noframes
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
   addStyle('#Panopto-Video-DL{position:fixed;top:10%;left:50%;width:70%;padding:2em 3em 1em;background-color:#2d3436;transform:translateX(-50%);z-index:1050}#Panopto-Video-DL *{margin-bottom:10px;color:#fff!important;font-size:18px;}#Panopto-Video-DL > div {margin-top: 1em;}#Panopto-Video-DL ul,#Panopto-Video-DL ol,#Panopto-Video-DL li{margin:0 .5em;padding:0 .5em;list-style:decimal}#Panopto-Video-DL button{margin-left:5px;margin-right:5px;color:#000!important;font-size:16px;}#Panopto-Video-DL p{margin-top:0.5em;}#Panopto-Video-DL input{color:black!important;}#Panopto-Video-DL textarea{width:100%;color:black!important;resize:vertical;white-space:nowrap;}')
 
-  const button = document.createElement('a');
-  button.href = '#';
-  button.innerHTML = '<span class="material-icons" style="font-size:15px;margin-bottom:-0.25rem;">file_download</span> Download';
-
   if (location.pathname.includes('/List.aspx')) {
+    const button = document.createElement('button');
+    button.className = 'css-t83cx2 css-tr3oo4 css-coghg4';
+    button.role = 'button';
+    button.style.marginLeft = '0.5rem';
+    button.innerHTML = '<span class="material-icons css-6xugel" style="font-size: 18px;margin-bottom:-0.25rem;">file_download</span>Download';
+
     button.addEventListener('click', e => {
       e.preventDefault();
       e.stopPropagation();
 
       let _t;
-      const list = (_t = document.querySelectorAll('#listTable tbody > tr a.list-title')).length ?
+      const list = (_t = document.querySelectorAll('#listViewContainer tbody > tr a.detail-title')).length ?
             _t : (_t = document.querySelectorAll('#detailsTable tbody > tr a.detail-title')).length ?
             _t : (_t = document.querySelectorAll('#thumbnailGrid > li a.detail-title')).length ?
             _t : null;
@@ -64,7 +66,7 @@
         return requestDeliveryInfo(videoId)
           .catch(error => {
             new Notify({
-              text: 'Failed to get lesson link for "'+videoTitle+'"',
+              text: 'Failed to get lesson link for "' + videoTitle + '"',
               type: 'error',
               timeout: null
             }).show();
@@ -73,28 +75,30 @@
 
       Promise.allSettled(requestsList)
         .then(responses => {
-        // log(responses)
-        n.close();
-        let copyText = '';
-        responses.forEach(response => {
-          if (response.status == 'fulfilled' && response.value) {
-            const streamUrl = response.value?.[0];
-            if (streamUrl)
-              copyText += streamUrl + '\n';
-          }
+          // log(responses)
+          n.close();
+          let copyText = '';
+          responses.forEach(response => {
+            if (response.status == 'fulfilled' && response.value) {
+              const streamUrl = response.value?.[0];
+              if (streamUrl)
+                copyText += streamUrl + '\n';
+            }
+          });
+          if (copyText !== '')
+            copyToClipboard(copyText);
         });
-        if (copyText !== '')
-          copyToClipboard(copyText);
-      });
     });
-    const li = document.createElement('li');
-    li.appendChild(button);
-    document.querySelector('ul.action-buttons').appendChild(li);
+
+    document.querySelector('#actionHeader button')?.parentElement.appendChild(button);
   }
   else if (location.pathname.includes('/Viewer.aspx')) {
     const url = new URL(location.href)
     const videoId = url.searchParams.get('id');
 
+    const button = document.createElement('a');
+    button.href = '#';
+    button.innerHTML = '<span class="material-icons" style="font-size:15px;margin-bottom:-0.25rem;">file_download</span> Download';
     button.classList = 'event-tab-header';
     button.style = 'display:inline-flex;align-items:center;position:absolute;bottom:30px;padding:5px 10px;text-decoration:none;cursor:pointer;';
     button.addEventListener('click', e => {
@@ -139,7 +143,7 @@
             const modal = showModal('<h2 style="font-size:20px;">Download another source video</h2><ul></ul><p style="text-align:center;"><button onclick="this.parentElement.parentElement.remove();">Close</button><button onclick="localStorage.setItem(\'other-source-viewed\', true);this.parentElement.parentElement.remove();">Close and don\'t show again</button></p>');
             streams.forEach((value, index) => {
               const li = document.createElement('li');
-              li.innerHTML = (value.Name?.replace(/-?(\d{8}T\d+Z)+((.)?(\w+))?/g, '').replace(/_/g, ' ') || 'Stream '+(index+1)) + '<button>Copy</button>';
+              li.innerHTML = (value.Name?.replace(/-?(\d{8}T\d+Z)+((.)?(\w+))?/g, '').replace(/_/g, ' ') || 'Stream ' + (index + 1)) + '<button>Copy</button>';
               li.querySelector('button').addEventListener('click', (e) => { copyToClipboard(value.StreamUrl); })
               modal.querySelector('ul').appendChild(li);
             });
@@ -162,13 +166,13 @@
   function requestDeliveryInfo(videoId) {
     return fetch(
       location.origin + '/Panopto/Pages/Viewer/DeliveryInfo.aspx', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json, text/javascript, */*; q=0.01',
-          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
-        },
-        body: 'deliveryId='+videoId+'&isEmbed=true&responseType=json',
-      })
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      },
+      body: 'deliveryId=' + videoId + '&isEmbed=true&responseType=json',
+    })
       .then(respose => respose.json())
       .then(data => {
         const errorCode = data.ErrorCode;
@@ -232,7 +236,7 @@
     }
   }
 
-  function log(message, level='log') {
+  function log(message, level = 'log') {
     console[level]('%c Panopto-Video-DL ->', 'color:red;font-size:14px;', message);
   }
 
